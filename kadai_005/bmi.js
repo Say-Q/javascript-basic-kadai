6bf94ed3d6209a57b5f7c2776bc867c77d35a513
Say-Q <say19840702@gmail.com>
2024-03-10 19:47:52 +0900

    kadai_005のjsを修正

--- kadai_005/bmi.js
+++ kadai_005/bmi.js
@@ -1,11 +1,9 @@
 let w_Kg;
 let h_M;
+let BMI;
 
-w_Kg=68;
-h_M=1.7;
-
-BMI = [w_Kg] / [h_M * h_M];
+w_Kg = 68;
+h_M = 1.7;
+BMI = w_Kg/(h_M*h_M);
 
 console.log(BMI);
-
-console.log([w_Kg] / [h_M * h_M]);
