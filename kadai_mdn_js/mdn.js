8f47d345ad816ac0322999e9f8c9ad9b941c31a8
Say-Q <say19840702@gmail.com>
2024-03-12 15:14:26 +0900

    課題mdn作成

--- kadai_mdn_js/mdn.js
+++ kadai_mdn_js/mdn.js
@@ -1,3 +1,6 @@
-const today = new Date().getFullYear;
+var today = new Date() ;
+    var to_Y = today.getFullYear();
+    var to_M = today.getMonth() + 1;
+    var to_D = today.getDate();
 
-console.log(today);
+console.log(to_Y + "年" + to_M + "月" + to_D + "日");
